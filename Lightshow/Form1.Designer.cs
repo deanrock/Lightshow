1c791548a274cc827eff747bf14ec62b5c702cdf
Dejan Levec <dejan@dejanlevec.com>
2011-06-26 09:26:43 +0200

    first commit

---
+++ Lightshow/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Lightshow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.modeTimer = new System.Windows.Forms.Timer(this.components);
+            this.light = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(198, 194);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(219, 194);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(240, 194);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(261, 194);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(15, 14);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(282, 194);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(15, 14);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(303, 194);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(15, 14);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(324, 194);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(15, 14);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(345, 194);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(15, 14);
+            this.checkBox8.TabIndex = 8;
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(366, 194);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(15, 14);
+            this.checkBox9.TabIndex = 9;
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(387, 194);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(15, 14);
+            this.checkBox10.TabIndex = 10;
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // modeTimer
+            // 
+            this.modeTimer.Tick += new System.EventHandler(this.modeTimer_Tick);
+            // 
+            // light
+            // 
+            this.light.Enabled = true;
+            this.light.Tick += new System.EventHandler(this.light_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer modeTimer;
+        private System.Windows.Forms.Timer light;
+    }
+}
+
---
+++ Lightshow/Form1.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using CoreAudioApi;
+
+namespace Lightshow
+{
+    public partial class Form1 : Form
+    {
+        private static System.IO.Ports.SerialPort serialPort1;
+        private MMDevice device;
+        static int _bits;
+        static int _device_bits;
+        int tbMaster;
+        private string previousStatusString = "";
+
+        private int CEILING_LIGHT = 7;
+
+        //knight rider
+        private int knightrider = 0;
+        private int knightriderDirection = 0;
+
+        private string mode = "music01110";
+
+        private string httpRequest(string url)
+        {
+            return "";
+            WebRequest request = WebRequest.Create (url);
+ 
+            // For HTTP, cast the request to HttpWebRequest
+            // allowing setting more properties, e.g. User-Agent.
+            // An HTTP response can be cast to HttpWebResponse.
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    // Ensure that the correct encoding is used. 
+                    // Check the response for the Web server encoding.
+                    // For binary content, use a stream directly rather
+                    // than wrapping it with StreamReader.
+
+                    using (StreamReader reader = new StreamReader
+                       (response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        string content = reader.ReadToEnd();
+                        // process the content
+                        return content;
+                    }
+
+                }
+            }
+            catch (Exception) { }
+
+            return "";
+        }
+
+          
+        
+
+        static void switchIt(int index, bool x)
+        {
+            
+
+            if (x)
+            {
+                _bits |= (1 << index);  //set bit index 1
+            }
+            else
+            {
+                _bits &= ~(1 << index); //set bit index 0  
+            }
+        }
+
+        static void switchItDevice(int index, bool x)
+        {
+
+
+            if (x)
+            {
+                _device_bits |= (1 << index);  //set bit index 1
+            }
+            else
+            {
+                _device_bits &= ~(1 << index); //set bit index 0  
+            }
+        }
+
+        void s(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10)
+        {
+            sw2(x6, x7, x8, x9, x10, 0);
+            sw2(x5, x4, x3, x2, x1, 1);
+
+            int[] status = new int[] { 0,0,0,0,0,0,0,0,0,0};
+
+            checkBox1.Checked = (x1 == 1) ? true : false;
+            status[0] = (x1 == 1) ? 1 : 0;
+            checkBox2.Checked = (x2 == 1) ? true : false;
+            status[1] = (x2 == 1) ? 1 : 0;
+            checkBox3.Checked = (x3 == 1) ? true : false;
+            status[2] = (x3 == 1) ? 1 : 0;
+            checkBox4.Checked = (x4 == 1) ? true : false;
+            status[3] = (x4 == 1) ? 1 : 0;
+            checkBox5.Checked = (x5 == 1) ? true : false;
+            status[4] = (x5 == 1) ? 1 : 0;
+            checkBox6.Checked = (x6 == 1) ? true : false;
+            status[5] = (x6 == 1) ? 1 : 0;
+            checkBox7.Checked = (x7 == 1) ? true : false;
+            status[6] = (x7 == 1) ? 1 : 0;
+            checkBox8.Checked = (x8 == 1) ? true : false;
+            status[7] = (x8 == 1) ? 1 : 0;
+            checkBox9.Checked = (x9 == 1) ? true : false;
+            status[8] = (x9 == 1) ? 1 : 0;
+            checkBox10.Checked = (x10 == 1) ? true : false;
+            status[9] = (x10 == 1) ? 1 : 0;
+
+
+
+            string statusString = "";
+            for (int i = 0; i < status.Length; i++)
+            {
+                statusString += status[i];
+            }
+
+
+            if (statusString != previousStatusString)
+            {
+                //send to server (only if the status has changed!)
+
+                httpRequest("http://192.168.1.108/variables/variables.php?action=set&key=lightshow&api=f43g56rewrq4t5&value=" + statusString);
+
+                previousStatusString = statusString;
+            }
+        }
+
+        static void sw2(int x1, int x2, int x3, int x4, int x5, int x)
+        {
+            switchIt(0, false);
+            if (x == 1)
+            {
+                switchIt(1, false);
+            }
+            else
+            {
+                switchIt(1, true);
+            }
+
+            
+            if (x1 == 1)
+            {
+                switchIt(7, true);
+            }
+            else
+            {
+                switchIt(7, false);
+            }
+
+            if (x2 == 1)
+            {
+                switchIt(6, true);
+            }
+            else
+            {
+                switchIt(6, false);
+            }
+
+            if (x3 == 1)
+            {
+                switchIt(5, true);
+            }
+            else
+            {
+                switchIt(5, false);
+            }
+
+            if (x4 == 1)
+            {
+                switchIt(4, true);
+            }
+            else
+            {
+                switchIt(4, false);
+            }
+
+            if (x5 == 1)
+            {
+                switchIt(3, true);
+            }
+            else
+            {
+                switchIt(3, false);
+            }
+
+
+            try
+            {
+                serialPort1.Write(new byte[] { (byte)_bits }, 0, 1);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    serialPort1.Open();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        void otherDevices(int device, bool state) {
+            switchItDevice(0, true);
+            switchItDevice(device, state);
+
+            try
+            {
+                serialPort1.Write(new byte[] { (byte)_device_bits }, 0, 1);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    serialPort1.Open();
+                }
+                catch (Exception) { }
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //COM3
+            System.ComponentModel.IContainer components = new System.ComponentModel.Container();
+            serialPort1 = new System.IO.Ports.SerialPort(components);
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 9600;
+
+            serialPort1.Open();
+            if (!serialPort1.IsOpen)
+            {
+                MessageBox.Show("Oops");
+            }
+
+            //Audio
+            MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
+            device = DevEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+            tbMaster = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            device.AudioEndpointVolume.OnVolumeNotification += new AudioEndpointVolumeNotificationDelegate(AudioEndpointVolume_OnVolumeNotification);
+            //System.ComponentModel.IContainer components = new System.ComponentModel.Container();
+        }
+
+        /* AUDIO 
+         * ============================================================================================== */
+        void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            if (this.InvokeRequired)
+            {
+                object[] Params = new object[1];
+                Params[0] = data;
+                this.Invoke(new AudioEndpointVolumeNotificationDelegate(AudioEndpointVolume_OnVolumeNotification), Params);
+            }
+            else
+            {
+                tbMaster = (int)(data.MasterVolume * 100);
+            }
+        }
+
+        private void tbMaster_Scroll(object sender, EventArgs e)
+        {
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = ((float)tbMaster / 100.0f);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (device!=null)
+            {
+                int value = (int)(device.AudioMeterInformation.MasterPeakValue * 100);
+
+
+
+                label1.Text = value.ToString();
+
+                //if (value > 40) value = 40;
+
+                //int real = value * 10 / 40;
+                
+
+
+                int[] z = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+               
+               //UV meter: 00111 11100
+                if (mode == "music01110")
+                {
+                    int real = value / 20;
+                    if (real > 0)
+                    {
+
+                        for (int i = 0; i < real; i++)
+                        {
+                            z[i] = 1;
+                        }
+
+
+
+                    }
+                    else
+                    {
+                    }
+
+                    s(z[4], z[3], z[2], z[1], z[0], z[0], z[1], z[2], z[3], z[4]);
+                }
+
+
+                if (mode == "music00100")
+                {
+                    
+                    //UV meter 00010000
+                      int real = value/10;
+                      if (real > 0)
+                      {
+                         // for (int i = 0; i < real; i++)
+                         // {
+                        //      z[i] = 1;
+                         // }
+                          try
+                          {
+                              z[real] = 1;
+                          }
+                          catch (Exception) { }
+                      }
+
+                      s(z[0], z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9]);
+                      
+                    
+                }
+
+               
+
+                //TEST s(1, 1, 1, 1, 0, 0, 1, 0, 0, 1);
+                //TEST s(1, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+            }
+
+
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //get mode
+
+            mode = httpRequest("http://192.168.1.108/variables/variables.php?action=get&key=lightshow_mode&api=f43g56rewrq4t5");
+
+            if (mode == "knightrider")
+            {
+                modeTimer.Enabled = true;
+            }
+            else
+            {
+                modeTimer.Enabled = false;
+            }
+
+            if (mode == "manual")
+            {
+                string stat ="";
+                //get status
+
+                stat=httpRequest("http://192.168.1.108/variables/variables.php?action=get&key=lightshow&api=f43g56rewrq4t5");
+
+
+                int[] z = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
+                for (int i = 0; i < stat.Length; i++)
+                {
+                    try
+                    {
+                        z[i] = Int32.Parse(stat[i].ToString());
+                    }
+                    catch (Exception) { }
+                }
+                
+                s(z[0], z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9]);
+            }
+
+            label2.Text = mode;
+        }
+
+        private void modeTimer_Tick(object sender, EventArgs e)
+        {
+            if (mode == "knightrider")
+            {
+                int[] z = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+                if (knightriderDirection == 0)
+                {
+                    knightrider++;
+
+                    if (knightrider > 9)
+                    {
+                        knightrider = 8;
+                        knightriderDirection = 1;
+                    }
+                }
+                else
+                {
+                    knightrider--;
+
+                    if (knightrider < 0)
+                    {
+                        knightrider = 2;
+                        knightriderDirection = 0;
+                    }
+                }
+
+                try
+                {
+                    z[knightrider] = 1;
+                }
+                catch (Exception) { }
+
+                s(z[0], z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9]);
+            }
+        }
+
+        private void light_Tick(object sender, EventArgs e)
+        {
+            string content = httpRequest("http://192.168.1.108/variables/variables.php?action=get&key=light&api=f43g56rewrq4t5");
+            content = "on";
+            if (content == "on")
+            {
+                otherDevices(CEILING_LIGHT, true);
+            }
+            else
+            {
+                otherDevices(CEILING_LIGHT, false);
+            }
+        }
+
+
+    }
+}
